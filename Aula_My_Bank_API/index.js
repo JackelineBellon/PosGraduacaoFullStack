c0f3a210136cd71ed683e8f921f3cf474013b549
JackelineBellon <jackeline.bellon@gmail.com>
2024-01-08 23:46:58 -0300

    Adicionado aula de construção de api com node, criado index, e classe router para criação de rotas

---
+++ Aula_My_Bank_API/index.js
@@ -0,0 +1,29 @@
+import express, { json } from "express";
+import accountRouter from "./routes/accounts.js";
+import { promises as fs } from "fs";
+
+const { readFile, writeFile } = fs;
+
+const app = express();
+app.use(express.json());
+
+app.use("/account", accountRouter);
+
+app.listen(3000, async () => {
+  try {
+    await readFile("accounts.json");
+    console.log("API Started");
+  } catch (err) {
+    const initialJson = {
+      nextId: 1,
+      accounts: [],
+    };
+    writeFile("accounts.json", JSON.stringify(initialJson))
+      .then(() => {
+        console.log("API Started and File Created!");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+});
